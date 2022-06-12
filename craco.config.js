09ca029beff371f17e2e828d2afebbcf22a33092
miles <a09112332@gmail.com>
2022-06-12 16:31:00 +0800

    header

---
+++ craco.config.js
@@ -0,0 +1,25 @@
+const CracoLessPlugin = require('craco-less');
+const path = require('path')
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#1DA57A' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      // 'components': path.resolve(__dirname, './src/components')
+    }
+  }
+}
+
