608fa36e2903e158f75578a4cf327c5bc9399a83
miles <a09112332@gmail.com>
2022-06-12 16:25:41 +0800

    header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { renderRoutes } from 'react-router-config'
+import { HashRouter } from 'react-router-dom'
+import routes from './router';
+
+import MSHeader from './components/header';
+import MSFooter from './components/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <MSHeader />
+      {renderRoutes(routes)}
+      <MSFooter />
+    </HashRouter>
   );
 }
 
